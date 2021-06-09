e3cc95bc7a827f01d899acffc55d3910b175a82b
adisharma132001 <adisharma132001@gmail.com>
2021-06-09 16:13:11 +0530

    day11 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,22 @@
+class CatalogModel{
+
+static final items=[
+   Item(
+     id:1,
+     name: " Apple iphone 12 pro", 
+     desc: "Apple iphone 12 Generation", 
+     price: 999,
+     color: "#33505a", 
+     image: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000")
+     ];
+}
+
+
+
+
+
 class Item{
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,5 +25,3 @@ class Item{
 
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
-
-final products=[ Item(id: id, name: name, desc: desc, price: price, color: color, image: image)]
