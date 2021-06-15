0330c3e90ef8f5200abc7dfcf871ef1ff47fb72e
adisharma132001 <adisharma132001@gmail.com>
2021-06-15 17:14:41 +0530

    day13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,16 @@
+import 'dart:html';
+
 import 'package:catalog/models/catalog.dart';
 import 'package:catalog/widgets/drawer.dart';
 import 'dart:convert';
 import 'package:catalog/widgets/item_widget.dart';
+import 'package:catalog/widgets/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+String velocityx() => "package:velocity_x/velocity_x.dart";
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -35,51 +42,56 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        
-        
-        title: Text("Catalog App",),
-        ),
-        body:Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: (CatalogModel.items!=  null && CatalogModel.items.isNotEmpty)
-          ?GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            ), 
-            itemBuilder: (context,index){
-              final item = CatalogModel.items[index];
-              return Card(
-                clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                child: GridTile(
-                  header: Container(
-                    child: Text(item.name,
-                    style: TextStyle(color: Colors.white),
-                    ),
-                    padding:  const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                    ),
-                  ),
-                  child: Image.network(item.image,),
-                  footer: Container(
-                    child: Text(item.price.toString(),
-                    style: TextStyle(color: Colors.white),
-                    ),
-                    padding:  const EdgeInsets.all(12),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                    ),
-                  ),
-                  ));
-            },
-            itemCount: CatalogModel.items.length,)
-          
-          : Center(child: CircularProgressIndicator(),),
-        ),
-      drawer: MyDrawer(),
+        body: SafeArea(
+            child: Container(
+              padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              CatalogHeader(),
+              if(CatalogModel.items!=null&& CatalogModel.items.isNotEmpty)
+              CatalogList()
+              else
+                Center(child: CircularProgressIndicator(),)
+            ]
+            ,),
+          ),
+        )
+      );
+  }
+}
+
+
+
+class CatalogHeader extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              "Catalog App".text.xl5.bold.color(MyTheme.darkbluish).make(),
+              "Trending Products".text.xl2.make(),
+            ],
+            );
+  }
+}
+
+
+
+
+class CatalogList extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context,index){
+        final catalog =CatalogModel.items[index];
+        return CatalogItem(catalog);
+      }
       );
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -3,7 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme{
   static ThemeData lightTheme(BuildContext context)=> ThemeData(primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color:Colors.white,
         elevation: 0.0,
@@ -16,4 +16,8 @@ class MyTheme{
         brightness: Brightness.dark,
       );
       
+ static Color creamcolor=Color(0xfff5f5f5);
+ static Color darkbluish=Color(0xff403b58);
+
+
 }
