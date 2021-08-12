070ac72e4993dd40cc00104d8083e2237a23e2e4
adisharma132001 <adisharma132001@gmail.com>
2021-08-12 23:17:38 +0530

    Day 16 completed

---
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -0,0 +1,19 @@
+import 'package:velocity_x/velocity_x.dart';
+import 'package:catalog/widgets/themes.dart';
+import 'package:flutter/material.dart';
+
+
+class CatalogHeader extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              "Catalog App".text.xl5.bold.color(MyTheme.darkbluish).make(),
+              "Trending Products".text.xl2.make(),
+            ],
+            );
+  }
+}
