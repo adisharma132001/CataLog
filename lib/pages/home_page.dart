b4ce366cff6563f55bc9b8b50e7b2fcfac775dc0
adisharma132001 <adisharma132001@gmail.com>
2021-08-23 15:25:31 +0530

    Day 27 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 
+import 'package:catalog/core/store.dart';
+import 'package:catalog/models/cart.dart';
 import 'package:catalog/models/catalog.dart';
 import 'package:catalog/utils/routes.dart';
 import 'package:catalog/widgets/home_widgets/catalog_header.dart';
@@ -10,6 +12,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart'as http;
 
 
 
@@ -25,6 +28,7 @@ class _HomePageState extends State<HomePage> {
   final int days =30;
 
   final String name="codepur";
+  final url ="https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
 
   @override
   void initState() {
@@ -34,7 +38,10 @@ class _HomePageState extends State<HomePage> {
 
   loadData() async {
     await Future.delayed(Duration(seconds: 2));
-    final catalogJson =await rootBundle.loadString("assets/files/catalog.json");
+    //final catalogJson =await rootBundle.loadString("assets/files/catalog.json");
+
+    final response =await http.get(Uri.parse(url));
+    final catalogJson = response.body;
     final decodedData = jsonDecode(catalogJson);
     var productData = decodedData["products"];
     print(productData);
@@ -44,31 +51,44 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
-      backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(onPressed: ()=>Navigator.pushNamed(context, MyRoutes.cartRoute),
-    backgroundColor: context.theme.buttonColor,
-      child: Icon(CupertinoIcons.cart,color: Colors.white,),),
+        backgroundColor: context.canvasColor,
+        floatingActionButton: VxBuilder(
+          mutations: {AddMutation, RemoveMutation},
+          builder: (BuildContext context, store, VxStatus? status) => FloatingActionButton(
+            onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+            backgroundColor: context.theme.buttonColor,
+            child: Icon(
+              CupertinoIcons.cart,
+              color: Colors.white,
+            ),
+          ).badge(
+              color: Vx.gray200,
+              size: 22,
+              count: _cart.items.length,
+              textStyle: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
         body: SafeArea(
-            child: Container(
-              padding: Vx.m32,
+          child: Container(
+            padding: Vx.m32,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-              CatalogHeader(),
-              if(CatalogModel.items!=null&& CatalogModel.items.isNotEmpty)
-              CatalogList().py16().expand()
-              else
-               CircularProgressIndicator().centered().expand(),
-            ]
-            ,),
+                CatalogHeader(),
+                if (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+                  CatalogList().py16().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            ),
           ),
-        )
-      );
+        ));
   }
 }
-
 
 
 
